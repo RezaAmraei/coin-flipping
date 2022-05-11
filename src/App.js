4b1cbccb78a25465bc04274c34afe8fac3b39bf7
Reza Amraei <amraeireza@gmail.com>
2022-05-11 00:17:36 -0700

    Coin Flip

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  function fiveHeads() {
+    return new Promise((resolve, reject) => {
+      // your code here!
+      let attempts = 0;
+      let headsCount = 0;
+      while (attempts < 100 && headsCount < 5) {
+        let coinFlip = Math.random() > 0.5 ? "heads" : "tails";
+        if (coinFlip == "heads") {
+          headsCount++;
+          attempts++;
+          if (headsCount == 5) {
+            resolve(`You flipped 5 heads in a row in ${attempts} attempts!`);
+          }
+        } else {
+          attempts++;
+          headsCount = 0;
+        }
+      }
+      reject("You failed to flip 5 heads in a row in 100 attemps");
+    });
+  }
+  fiveHeads()
+    .then((res) => console.log(res))
+    .catch((err) => console.log(err));
+  console.log("When does this run now?");
+
   return (
     <div className="App">
       <header className="App-header">
